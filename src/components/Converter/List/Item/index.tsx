db3cc4ecda551fabb52cd2b9892f7babd374ab95
ViniciusGFerreira <vinicius.gf@outlook.com>
2023-05-24 14:26:48 -0300

    handle negative sign and minimum temps

--- src/components/Converter/List/Item/index.tsx
+++ src/components/Converter/List/Item/index.tsx
@@ -1,5 +1,6 @@
 import { Temp } from "..";
 import { celsiusToFahrenheit, celsiusToKelvin, fahrenheitToCelsius, fahrenheitToKelvin, kelvinToCelsius, kelvinToFahrenheit } from "../../../../common/converter/converter";
+import InfoButton from "./InfoButton";
 import styles from './Item.module.scss'
 
 interface Props {
@@ -10,33 +11,61 @@ interface Props {
 }
 
 
-
 export default function Item({ label, temp, minimumTemp, setTemp }: Props) {
+    function setEmptyTemp() {
+        setTemp({
+            celsius: '',
+            fahrenheit: '',
+            kelvin: ''
+        })
+    }
+
     function changeAllTemp(event: React.ChangeEvent<HTMLInputElement>) {
-        if (isNaN(Number(event.target.value))) { console.log('if'); return }
+        if (event.target.value == '-') {
+            setTemp({
+                celsius: '',
+                fahrenheit: '',
+                kelvin: '',
+                [event.target.id]: event.target.id === 'kelvin' ? 'invalid' : '-'
+            })
+            return
+        }
+
+        const unit = Number(event.target.value);
+
+        if (isNaN(unit)) { console.log('NaN'); return; }
+
+        if (unit < minimumTemp) {
+            //setEmptyTemp();
+            setTemp({
+                celsius: '',
+                fahrenheit: '',
+                kelvin: '',
+                [label]: 'invalid'
+            })
+            return;
+        }
+
         switch (event.target.id) {
             case 'celsius':
-                const celsius = Number(event.target.value)
                 setTemp({
-                    celsius: celsius,
-                    fahrenheit: celsiusToFahrenheit(celsius),
-                    kelvin: celsiusToKelvin(celsius)
+                    celsius: unit,
+                    fahrenheit: celsiusToFahrenheit(unit),
+                    kelvin: celsiusToKelvin(unit)
                 })
                 break;
             case 'fahrenheit':
-                const fahrenheit = Number(event.target.value)
                 setTemp({
-                    celsius: fahrenheitToCelsius(fahrenheit),
-                    fahrenheit: fahrenheit,
-                    kelvin: fahrenheitToKelvin(fahrenheit)
+                    celsius: fahrenheitToCelsius(unit),
+                    fahrenheit: unit,
+                    kelvin: fahrenheitToKelvin(unit)
                 })
                 break;
             case 'kelvin':
-                const kelvin = Number(event.target.value)
                 setTemp({
-                    celsius: kelvinToCelsius(kelvin),
-                    fahrenheit: kelvinToFahrenheit(kelvin),
-                    kelvin: kelvin
+                    celsius: kelvinToCelsius(unit),
+                    fahrenheit: kelvinToFahrenheit(unit),
+                    kelvin: unit
                 })
         }
     }
@@ -47,11 +76,11 @@ export default function Item({ label, temp, minimumTemp, setTemp }: Props) {
                 {label.toUpperCase()}
             </div>
             <div className={styles.temp}>
+                <InfoButton minimumTemp={minimumTemp} label={label} />
                 <input
                     className={styles.item__input}
-                    type="number"
+                    type="text"
                     id={label}
-                    min={minimumTemp}
                     value={temp[label]}
                     onChange={e => {
                         changeAllTemp(e)
